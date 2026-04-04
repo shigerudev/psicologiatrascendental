1f42b67da58b5e13d2e26a8a279d38157985b1b6
shigerubt <z648s.7bt@gmail.com>
2026-04-04 04:05:46 -0600

    feat: implement interactive game interface and Firebase initialization for the transcendental psychology platform

---
+++ frontend/js/firebase-init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0CoPJ3IJR0k_6GSq-AsftyIpLdneK1s4",
+  authDomain: "psicologiatrascendental.firebaseapp.com",
+  projectId: "psicologiatrascendental",
+  storageBucket: "psicologiatrascendental.firebasestorage.app",
+  messagingSenderId: "323328716378",
+  appId: "1:323328716378:web:1cb4cefa51c84e02567263"
+};
+
+export const app = initializeApp(firebaseConfig);
